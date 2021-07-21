92e877b15879c5c0c7614328004b7e06e7b300a1
perfectimhetsike.igbadumhe@stanbicibtc.com <PerfectImhetsike.Igbadumhe@stanbicibtc.com>
2021-07-22 00:18:14 +0100

    add entity frame work to project and in memory database and make sure it only works in development environment

--- Platform/Startup.cs
+++ Platform/Startup.cs
@@ -1,6 +1,8 @@
+using DataStore.EF;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Platform.Filter;
@@ -15,8 +17,21 @@ namespace Platform
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private readonly IWebHostEnvironment _env;
+        public Startup(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            if (_env.IsDevelopment())
+            {
+                services.AddDbContext<BugsContext>(options =>
+                {
+                    options.UseInMemoryDatabase("Bugs");
+                });
+            }
+            
             services.AddControllers(opt=>
             {
                 opt.Filters.Add<VersionDiscontinueResourceFilter>();
@@ -24,11 +39,15 @@ namespace Platform
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, BugsContext context)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                //create in memory database data for dev environment
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
             }
 
             app.UseRouting();
